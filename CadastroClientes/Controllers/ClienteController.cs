9b1b80d4e2b3e720479085ae985c17c2c75a360d
Viviane <viviteodoro19@gmail.com>
2022-12-12 18:50:06 -0300

    first commit

---
+++ CadastroClientes/Controllers/ClienteController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CadastroClientes.Controllers
+{
+    public class ClienteController : Controller
+    {
+        // GET: ClienteController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: ClienteController/Details/5
+        public ActionResult Details(int id)
+        {
+            //Exibir dados inseridos a partir do método create na View Index
+            return View();
+        }
+
+        // GET: ClienteController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ClienteController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                //Receber dados da View
+                //Salvar em memória 
+                //Redirecionar para Index
+                //Exibir novos dados
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ClienteController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ClienteController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                //Receber id cliente da View Index - Botão Alterar
+                //Retornar View Cadastrar cliente com dados preenchidos
+                //Salvar alterações em memória
+                //Redirecionar para Index
+                //Exibir novos dados
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ClienteController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ClienteController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                //Receber id cliente da View Index - Botão Excluir
+                //Exibir pop up de confirmação
+                //Se sim, excluir dados da memória
+                //Se não, voltar para Index e manter dados
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ CadastroClientes/Models/ClienteModel.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace CadastroClientes.Models
+{
+    public class ClienteModel
+    {
+        public int IdCliente { get; set; }
+        public string? NomeCliente { get; set; }
+
+        public string? CpfCliente { get; set; }
+
+        public List<Endereco>? EnderecoCliente { get; set; }
+    }
+
+    public class Endereco
+    {
+        public string? Logradouro { get; set; }
+        public int Numero { get; set; }
+        public string? Complemento { get; set; }
+        public string? Bairro { get; set; }
+        public string? Cidade { get; set; }
+        public string? Estado { get; set; }
+    }
+}
